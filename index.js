a8af74a42d10a1a70a99f6e609b10f9d210261bb
Shreyas Patel <shreyas.p@crestinfosystems.com>
2020-03-19 15:25:49 +0530

    Updated mysql db conf

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var chat = require('rs-chat')(app);
 chat.init({
   host     : '127.0.0.1',
   user     : 'root',
-  password : 'welcome',
+  password : 'nodedemo',
   database : 'rs_chat',
   assets   : 'assets'
 });
@@ -13,11 +13,11 @@ chat.init({
 app.listen(8080);
 
  
-console.log('Server started at 8080');
-console.log('Now you have two end point: http://localhost:8080/index.html');
-console.log('Second end point is http://localhost:8080/index2.html');
-console.log('Open both end point in different browser and your chat is ready to deploy');
-console.log('Have fun :)');
+// console.log('Server started at 8080');
+// console.log('Now you have two end point: http://localhost:8080/index.html');
+// console.log('Second end point is http://localhost:8080/index2.html');
+// console.log('Open both end point in different browser and your chat is ready to deploy');
+// console.log('Have fun :)');
 
 
 function handler (req, res) {
