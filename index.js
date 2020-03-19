d3c41fd666c4dcb97eca963277cd1070e095e490
Shreyas Patel <shreyas.p@crestinfosystems.com>
2020-03-19 15:01:30 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var app = require('http').createServer(handler);
+var fs = require('fs');
+var chat = require('rs-chat')(app);
+
+chat.init({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'welcome',
+  database : 'rs_chat',
+  assets   : 'assets'
+});
+
+app.listen(8080);
+
+ 
+console.log('Server started at 8080');
+console.log('Now you have two end point: http://localhost:8080/index.html');
+console.log('Second end point is http://localhost:8080/index2.html');
+console.log('Open both end point in different browser and your chat is ready to deploy');
+console.log('Have fun :)');
+
+
+function handler (req, res) {
+
+    var filePath = '.' + req.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
